e513aad634bbb1daadad6a38eeb0ffd077087705
Hannah Warren <hannahkwarren@gmail.com>
2021-12-14 19:59:12 -0500

    Adding WIP user story 3 before submit deadline

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,10 @@ class MoviesController < ApplicationController
         @movie = Movie.find(params[:id])
         @actors = @movie.actors_by_age
         @avg_age = @movie.average_actor_age.to_i
+
+        if params[:search].present?
+            @search_actors = Actor.search(params[:search])
+        end
     end
     
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Movies show" do
     
-    # As a user,
-# When I visit a movie's show page.
-# I see the movie's title, creation year, and genre,
-# and a list of all its actors from youngest to oldest.
-# And I see the average age of all of the movie's actors
-
     it "provides movie's title, creation year, and genre w/ actors" do
     
         studio1 = Studio.create!(name: "MGM", location: "Hollywood Hills, CA")
@@ -28,5 +22,36 @@ RSpec.describe "Movies show" do
         expect(page).to_not have_content("#{movie2.title}")
         expect(page).to_not have_content("#{movie2.creation_year}")
         expect(page).to_not have_content("#{movie2.genre}")
+    end
+
+#     Story 3
+# Add an Actor to a Movie
+
+# As a user,
+# When I visit a movie show page,
+# I do not see any actors listed that are not part of the movie
+# And I see a form to add an actor to this movie
+# When I fill in the form with the name of an actor that exists in the database
+# And I click submit
+# Then I am redirected back to that movie's show page
+# And I see the actor's name is now listed
+# (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
+
+    it 'associates searched actors with a movie' do 
+        studio1 = Studio.create!(name: "MGM", location: "Hollywood Hills, CA")
+        movie1 = studio1.movies.create!(title: "West Side Story", creation_year: 1961, genre: "Musical drama")
+
+        rita = movie1.actors.create!(name: "Rita Moreno", age: 75)
+        natalie = Actor.create!(name: "Natalie Wood", age: 43)
+
+        visit "/movies/#{movie1.id}"
+        expect(page).to have_content(@rita.name)
+        expect(page).to_not have_content(@natalie.name)
+
+        fill_in :search, with: "Nat"
+        expect(page).to have_content(@natalie.name)
+
+        click_link "Add"
+
     end
 end
